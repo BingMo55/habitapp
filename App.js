40433a5da6a974ad66ce6d545d9816e129abfca5
bing <bingmo55@gmail.com>
2021-01-30 12:54:59 -0800

    checking crossplatform sync

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {Image, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import logo from './assets/icon.png'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source = {logo} style={styles.logo} />
+      <Text style={styles.instructions}>
+        Open up App.js to start working on your app! lol
+        </Text>
       <StatusBar style="auto" />
+      <TouchableOpacity 
+      onPress={() => alert('Hello, world!')}
+      style={styles.button}>
+      <Text style={styles.buttonText}>pick a photo</Text>
+      </TouchableOpacity>
     </View>
+    
   );
 }
 
@@ -15,7 +24,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: 'center',
   },
+  logo:{
+    width: 305,
+    height: 189,
+    marginBottom:10,
+  },
+  instructions:{
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button:{
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff'
+  }
+
 });
